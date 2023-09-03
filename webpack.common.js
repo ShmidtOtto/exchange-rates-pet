05ac3fa345df33603cb734ca6ba3283943f233e2
Отто Шмидт <oschmidt>
2023-09-03 16:58:46 +0700

    Add relations

---
+++ webpack.common.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: path.resolve('src/index.tsx'),
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader", {
+                    loader: "postcss-loader",
+                    options: {
+                        postcssOptions: {
+                            ident: 'postcss',
+                            plugins: [tailwindcss, autoprefixer]
+                        }
+                    }
+                }],
+                exclude: /node_modules/
+            },
+            {
+                type: 'assets/resource',
+                use: 'assets/resource',
+                test: /\.(jpg|png|svg|gif|woff|woff2|eot|ttf)$/
+            }
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: path.resolve('src/public/manifest.json'), to: path.resolve('dist') },
+                { from: path.resolve('src/public/robots.txt'), to: path.resolve('dist') },
+            ],
+        }),
+        new HtmlPlugin({
+            title: 'exchange-rates-pet',
+            template: path.resolve('src/public/index.html'),
+            filename: 'index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        alias: {
+            "@components": path.resolve('src/components'),
+        }
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+        },
+    },
+    output: {
+        filename: '[name].js'
+    },
+    devServer: {
+        port: 3030,
+    },
+}
